c6a4a7955f90feef06102b285cda0b9b692053fa
George Kraychik <george.kraychik@gmail.com>
2013-08-15 21:57:39 +0400

    Addr filtered

--- scripts/make.sh
+++ scripts/make.sh
@@ -4,4 +4,4 @@
 #echo "Download of the main page complete."
 bash wget.sh http://ru.leonbets.com/
 bash delspaces.sh
-sed -e 's/<div[[:space:]]*class="lineb">[[:space:]]*Футбол[[:space:]]*<\/div>.*<a href="\([^"]*\)[^>]*\([^<]*\)<\/a>/\1\n\2/g' tmp/nospaces.html >tmp/extraction
+bash extract.sh
