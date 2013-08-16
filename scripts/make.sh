3998936f8bfa59056ef61a543c35b6825d8c8b71
George Kraychik <george.kraychik@gmail.com>
2013-08-16 15:44:28 +0400

    Intermidiate commit

--- scripts/make.sh
+++ scripts/make.sh
@@ -2,7 +2,9 @@
 #bash wget.sh http://ru.leonbets.com/
 #bash delspaces.sh
 #bash extract.sh
-#mkdir -p ../data/`date +%F`/`date +%H:%M:%S`
+d=$(date +%F)
+t=$(date +%H:%M:%S)
+mkdir -p ../data/"$d"/"$t"
 #Next follows data download
 #mkdir -p tmp/downloads
 cd tmp/downloads/
@@ -10,5 +12,14 @@ cd tmp/downloads/
 #wget -i ../addr -a ../../logs/wget_log
 #echo "Download complete."
 #Next follows the data extraction
-ls >lol
+x="0"
+for name in *
+do
+	if [ -f "$name" ]
+	then
+		grep -o '<h1>[^<]*</h1>' $name | sed -e 's/<h1>\([^<]\)*<\/h1>/\1/g' >../../../data/"$d"/"$t"/"$x"
+		x=$(($x + 1))
+	fi
+done
+
 cd ../../
