bc222ac60b819c6036a348b4874fbf55aeba4884
George Kraychik <george.kraychik@gmail.com>
2013-08-16 14:53:54 +0400

    Pages with information are downloaded. Before main extraction.

--- scripts/make.sh
+++ scripts/make.sh
@@ -1,7 +1,14 @@
 #! /bin/bash
-#echo "Download of the main page started..."
-#wget -a logs/wget_log -O tmp/result.html http://ru.leonbets.com/
-#echo "Download of the main page complete."
-bash wget.sh http://ru.leonbets.com/
-bash delspaces.sh
-bash extract.sh
+#bash wget.sh http://ru.leonbets.com/
+#bash delspaces.sh
+#bash extract.sh
+#mkdir -p ../data/`date +%F`/`date +%H:%M:%S`
+#Next follows data download
+#mkdir -p tmp/downloads
+cd tmp/downloads/
+#echo "Starting download of files. It will take much time..."
+#wget -i ../addr -a ../../logs/wget_log
+#echo "Download complete."
+#Next follows the data extraction
+ls >lol
+cd ../../
