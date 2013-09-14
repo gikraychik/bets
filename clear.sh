13fee76e9647fb3721815b304e009fcf8f3bfdcb
George Kraychik <george.kraychik@gmail.com>
2013-09-14 20:52:14 +0400

    merge.cpp written. Executable file - merge.out

---
+++ clear.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+cd scripts
+rm coeff commands_soon commands_today dates dates_soon dates_today empty filled
--- marathonbet.sh
+++ marathonbet.sh
@@ -4,14 +4,14 @@ echo "*************************************Bets extraction**********************
 cd scripts/
 mkdir -p tmp/
 mkdir -p logs/
-#bash wget.sh http://www.marathonbet.com/ru/betting/Football/
+bash wget.sh http://www.marathonbet.com/ru/betting/Football/
 bash delspaces.sh tmp/result.html tmp/nospaces.html
 echo "Extraction started..."
-grep -o '<div class="member-name">[^<]*</div>' < tmp/nospaces.html | sed -e 's/<div class="member-name">\([^<]*\)<\/div>/\1/g' >>commands_soon
-grep -o '<div class="today-member-name">[^<]*</div>' < tmp/nospaces.html | sed -e 's/<div class="today-member-name">\([^<]*\)<\/div>/\1/g' >>commands_today
-grep -o '<td class="date">[^<]*</td>' <tmp/nospaces.html  | sed -e 's/<td class="date">\([^<]*\)<\/td>/\1/g' >>dates
-grep '[а-яА-Я]' < dates > dates_soon
-grep -v '[а-яА-Я]' < dates > dates_today
+#grep -o '<div class="member-name">[^<]*</div>' < tmp/nospaces.html | sed -e 's/<div class="member-name">\([^<]*\)<\/div>/\1/g' >>commands_soon
+grep -o '<div class="[t]*[o]*[d]*[a]*[y]*[-]*member-name">[^<]*</div>' < tmp/nospaces.html | sed -e 's/<div class="[t]*[o]*[d]*[a]*[y]*[-]*member-name">\([^<]*\)<\/div>/\1/g' >commands
+grep -o '<td class="date">[^<]*</td>' <tmp/nospaces.html  | sed -e 's/<td class="date">\([^<]*\)<\/td>/\1/g' >dates
+#grep '[а-яА-Я]' < dates > dates_soon
+#grep -v '[а-яА-Я]' < dates > dates_today
 grep -o '<span class="selection-link coeff[^"]*">[^<]*</span>' <tmp/nospaces.html | sed -e 's/<span class="selection-link coeff[^"]*">\([^<]*\)<\/span>/\1/g' > coeff
 grep -o -n '<td class="js-price' < tmp/result.html | sed -e 's/^\([0-9][0-9]*\).*/\1/g' > filled
 grep -o -n 'td-min-width' < tmp/result.html | sed -e 's/^\([0-9][0-9]*\).*/\1/g' > empty
@@ -89,16 +89,28 @@ while read line
 do
 	new+=("$line")
 done < new_coeff
+grep -o '(\([0-9.-+][0-9.-+]*\))<br/>' < tmp/result.html | sed -e 's/(\([0-9.-+][0-9.-+]*\))<br\/>/\1/g' > bonuses
 
-fil=$(cat filled | wc -l)
-emp=$(cat empty | wc -l)
-ks=$(cat empty | wc -l)
-sum=$((($fil+$emp)*10))
-if [ "$sum" -eq "$ks" ]
+comm=$(cat commands | wc -l)
+coeff=$(cat new_coeff | wc -l)
+sum=$((5*$comm))
+cd ../data
+mkdir -p marathonbet.com
+d=$(date +%F)
+t=$(date +%H:%M:%S)
+if [ "$sum" -ne "$coeff" ]
 then
 	echo "Error accured! Control sum differs."
+	rm new_coeff
 	exit 1
 fi 
+cd marathonbet.com
+mkdir -p "$d"/"$t"
+cd "$d"/"$t"
+mv ../../../../scripts/commands commands
+mv ../../../../scripts/new_coeff coeff
+mv ../../../../scripts/bonuses bonuses
+cd ../../../../scripts/
 
 #for ((i=0; i<${#vals[*]};i++))
 #do
