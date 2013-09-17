cacd8e1e05c7c2787a762eff8c198c26b128c6a9
George Kraychik <george.kraychik@gmail.com>
2013-09-17 14:41:16 +0400

    wget.sh is now more stable

--- scripts/wget.sh
+++ scripts/wget.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#$1 - adress you download file from
+#$2 - directory used to download files
 echo "Download started...($1)"
-wget -a logs/wget_log -O "$2"/result.html $1
+wget -a logs/wget_log -w 5 -O "$2"/result.html $1
 echo "Download complete."
