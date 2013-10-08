388c4d78d07027a17e98774fe07ee427832d3403
George Kraychik <george.kraychik@gmail.com>
2013-10-09 00:33:35 +0400

    Dev

--- analis/analis.cpp
+++ analis/analis.cpp
@@ -1,12 +1,34 @@
 #include <iostream>
+#include <fstream>
 #include <stdio.h>
 #include <vector>
 #include <string>
 #include <dirent.h>
 #include <string.h>
+#include <cstdlib>
 
 using namespace std;
-
+class Result
+{
+public:
+	Result(void) : res1(NULL), res2(NULL)
+	{
+		for (int i = 0, j = 0; i<2,j<2; i++,j++) { v[i][j] = 0; }
+	}
+	Result(int x11, int x12, int x21, int x22) : res1(NULL), res2(NULL)
+	{
+		v[0][0] = x11; v[0][1] = x12; v[1][0] = x21; v[1][1] = x22;
+	}
+	Result(int **arr) : res1(NULL), res2(NULL)
+	{
+		for (int i=0, j=0; i < 2,j < 2; i++, j++) { v[i][j] = arr[i][j]; }
+	}
+	int v[2][2];
+	const char *res1;
+	const char *res2;
+private:
+	void transform(void);
+};
 class Date
 {
 public:
@@ -23,6 +45,7 @@ class Time
 public:
 	Time (const char *time) : h(0), m(0), s(0), time(string(time)) {}
 	Time (int h, int m, int s) : h(h), m(m), s(s), time(string("")) {}
+	Time (int h, int m) : h(h), m(m), s(0), time(string("")) {}
 	Time (void) : h(0), m(0), s(0), time(string("")) {}
 	int h, m, s;
 	string time;
@@ -51,7 +74,7 @@ class Line
 public:
 	Line(void) : date(Date()), time(Time()), coeff(10, 0), bonuses(4, 0) {}
 	Line(Date &date, Time &time, vector<double> &coeff, vector<double> &bonuses)
-		: date(date), time(time), coeff(coeff), bonuses(bonuses) {}	
+		: date(date), time(time), coeff(coeff), bonuses(bonuses) {}
 	Date date;
 	Time time;
 	vector<double> coeff;
@@ -63,7 +86,39 @@ public:
 	Match(void) : stinf(StaticInfo()), lines(vector<Line>()) {}
 	Match(const char *path)
 	{
-		
+		string ln;
+		ifstream file(path);
+		if (file.is_open())
+		{
+			while (1)
+			{
+				getline(file, ln);
+				int d = atoi(ln.data());
+				getline(file, ln);
+				int m = atoi(ln.data());
+				getline(file, ln);
+				int y = atoi(ln.data());
+				getline(file, ln);
+				int hours = atoi(ln.data());
+				getline(file, ln);
+				int minutes = atoi(ln.data());
+				getline(file, ln);
+				const char *com1 = ln.data();
+				getline(file, ln);
+				const char *com2 = ln.data();
+				Date date(d, m, y);
+				Time time(hours, minutes);
+				Commands cmds(com1, com2);
+				StaticInfo staticinfo(date, time, cmds);
+				int arr[2][2];
+				for (int i=0,j=0; i<2,j<2; i++,j++)
+				{
+					getline(file, ln);
+					arr[i][j] = atoi(ln.data());
+				}							
+			}
+			file.close();
+		}
 	}
 	StaticInfo stinf;
 	vector<Line> lines;
@@ -82,7 +137,7 @@ public:
 		{
 			if (strcmp(ent->d_name, ".") && strcmp(ent->d_name, ".."))
 			{
-				games.push_back(Match(ent->d_name));
+				games.push_back(Match((string("Matches/")+string(ent->d_name)).data()));
 			}
 		}
 	}
@@ -93,6 +148,6 @@ int main(int argc, char **argv)
 {
 	if (argc > 2) { return 1; }
 	const char *path = argv[1];
-	Analis analis(path);
+	Match m(path);
 	return 0;
 }
