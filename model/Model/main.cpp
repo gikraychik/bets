089f9a05810572c99e50bbd195462fc935a4e4ad
George Kraychik <george.kraychik@gmail.com>
2013-10-20 16:23:51 +0400

    Modified

--- model/Model/main.cpp
+++ model/Model/main.cpp
@@ -17,8 +17,9 @@ public:
     {
         return x;
     }
-    int N(int maxN) const  //если вернули (maxN - 1) => иммем возможность сделать ставку номер maxN
+    int N(int maxN, double &gain) const  //если вернули (maxN - 1) => иммем возможность сделать ставку номер maxN
     {
+        gain = 0.0;
         double copy_s = s;
         double y;
         int N = maxN - 1;
@@ -30,6 +31,7 @@ public:
             if (result == 1)
             {
                 copy_s += y * (k - 1);
+                if (copy_s >= s) { gain += (copy_s - s); copy_s = s; }
             }
             else
             {
@@ -38,28 +40,28 @@ public:
         }
         return N;
     }
-    double will_survive(int maxN)  //вероятность того, что сможем сделать шаг maxN
+    double will_survive(int n, double &average_gain)  //вероятность того, что сможем сделать шаг n; average_gain - средний заработок при условии, что дотянули
     {
         const int all = 100000;
-        const int size = maxN;
-        vector <double> P(size, 0);
+        int counter = 0;
+        double global_gain = 0.0;
+        double gain = 0.0;
         for (int i = 1; i < all; i++)
         {
-            int number = N(maxN);
-            P[number] += 1;
+            gain = 0.0;
+            int number = N(n, gain);
+            if (number == n - 1) { global_gain += gain; counter++; }
         }
-        /*for (int i = 0; i < size; i++)
-        {
-            //P[i] /= (double)all;
-            //cout << "N = " << i << " -> " << (double)P[i] / (double)all << endl;
-        }*/
-        return P[maxN - 1] / all;
+        average_gain = global_gain / counter;
+        return (double)counter / (double)all;
     }
     void print_survival_table(int N = 100)
     {
-        for (int i = 1; i < N; i++)
+        double average_gain;
+        for (int i = 1; i <= N; i++)
         {
-            cout << "n = " << i << " -> " << will_survive(i) << endl;
+            //average_gain = 0.0;
+            cout << "n = " << i << " -> " << will_survive(i, average_gain) << " Average Gain: " << (double)average_gain << endl;
         }
     }
 
@@ -91,9 +93,14 @@ int main(int argc, char *argv[])
 {
     srand(time(NULL));
     //Model model(1000, 3.2, 0.329843, 300);
-    Model model (1000, 3.2, 0.329843, 100);
+    Model model (3.2, 0.329843, 100.0, 20);
     //model.mkmodel(30);
     model.print_survival_table(100);
+    double gain = 0.0;
+
+    //model.will_survive(10, gain);
+    //cout << gain << endl;
+
     /*const int c = 100000;
     double zer = 0.0;
     double ones = 0.0;
