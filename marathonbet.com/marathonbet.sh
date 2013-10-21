fc97639641b1892f72f3af2da59e7ed49ad730be
George Kraychik <george.kraychik@gmail.com>
2013-10-21 16:32:17 +0400

    For some days the program was not working. Deleted word "coeff"

--- marathonbet.com/marathonbet.sh
+++ marathonbet.com/marathonbet.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 echo "Processing marathonbet.com."
+mkdir -p data/
 cd scripts/
 mkdir -p tmp/
 mkdir -p logs/
@@ -8,7 +9,7 @@ bash delspaces.sh tmp/result.html tmp/nospaces.html
 echo "Extraction started..."
 grep -o '<div class="[t]*[o]*[d]*[a]*[y]*[-]*member-name">[^<]*</div>' < tmp/nospaces.html | sed -e 's/<div class="[t]*[o]*[d]*[a]*[y]*[-]*member-name">\([^<]*\)<\/div>/\1/g' >commands
 grep -o '<td class="date">[^<]*</td>' <tmp/nospaces.html  | sed -e 's/<td class="date">\([^<]*\)<\/td>/\1/g' >dates
-grep -o '<span class="selection-link coeff[^"]*">[^<]*</span>' <tmp/nospaces.html | sed -e 's/<span class="selection-link coeff[^"]*">\([^<]*\)<\/span>/\1/g' > coeff
+grep -o '<span class="selection-link[^"]*">[^<]*</span>' <tmp/nospaces.html | sed -e 's/<span class="selection-link[^"]*">\([^<]*\)<\/span>/\1/g' > coeff
 grep -o -n '<td class="js-price' < tmp/result.html | sed -e 's/^\([0-9][0-9]*\).*/\1/g' > filled
 grep -o -n 'td-min-width' < tmp/result.html | sed -e 's/^\([0-9][0-9]*\).*/\1/g' > empty
 grep -o '(\([-0-9\.+][-0-9\.+]*\))<br/>' < tmp/result.html | sed -e 's/(\([-0-9\.+][-0-9\.+]*\))<br\/>/\1/g' > bonuses
