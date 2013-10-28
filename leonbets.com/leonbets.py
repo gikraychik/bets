14dc0744edd102c9bdccb07c97f8ad9b89065bb1
Kotov Maxim <maxpilgrim94@gmail.com>
2013-10-28 23:27:22 +0400

    Output format changed. 1 line = time; 2 line = commands; 3 line = bets

--- leonbets.com/leonbets.py
+++ leonbets.com/leonbets.py
@@ -17,6 +17,7 @@ def getInfo(data):
     i = 0
     for s in data:
         result = result + s[0].text_content().strip() + " "
+        if i == 0: result = result + "\n"
         if i == 3: break
         i = i + 1
     return result
@@ -34,11 +35,12 @@ def getTime(data):
 
 # print "Starting"
 output = open(outputFile, "w")
-links = docMain.get_element_by_id("lg1").iterlinks()
+links = docMain.get_element_by_id("lg1").iterlinks()  # getting links on leagues
 for link in links:
-    output.write("------------------\n")
+    # output.write("------------------\n")
     page = urllib.urlopen(link[2])
     doc = lxml.html.document_fromstring(page.read())
+    # getting info from specific league
     matches = doc.find_class("row1")
     for i in range(0, len(matches)):
         # print lxml.html.tostring(matches[i])
@@ -46,7 +48,7 @@ for link in links:
         if info[:5] != "print":
             continue
         else:
-            info = getTime(info) + " " + getInfo(matches[i].iterlinks())
+            info = getTime(info) + "\n" + getInfo(matches[i].iterlinks())
             output.write(info.encode("UTF-8"))
             output.write("\n")
             
@@ -57,7 +59,7 @@ for link in links:
         if info[:5] != "print":
             continue
         else:
-            info = getTime(info) + " " + getInfo(matches[i].iterlinks())
+            info = getTime(info) + "\n" + getInfo(matches[i].iterlinks())
             output.write(info.encode("UTF-8"))
             output.write("\n")
 
