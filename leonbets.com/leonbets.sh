4623fb08f89a5ed2aa57c424835143843adf4394
Kotov Maxim <maxpilgrim94@gmail.com>
2013-10-28 23:03:47 +0400

    leonbets.sh parametrs added

--- leonbets.com/leonbets.sh
+++ leonbets.com/leonbets.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 echo "Processing leonbets.com"
-d="2013-10-28"
-t="22:10:30"
+# d - date, first parametr
+# t - time, second parametr
+d=$1 
+t=$2
 
 mkdir -p ../data/$d/$t/leonbets.com/
 touch ../data/$d/$t/leonbets.com/log.txt
